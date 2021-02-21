ade4ab8559a660c299515c6da241e7fc8e2c4c29
aleksander73 <olekdobkowski@gmail.com>
2021-02-21 02:39:27 +0100

    Include message in response on register fail

--- src/api/UserRouter.js
+++ src/api/UserRouter.js
@@ -3,8 +3,8 @@ const { userService } = require('../services');
 
 const router = express.Router();
 router.post('/register', async (req, res) => {
-    let result = await userService.registerUser(req.body.username, req.body.email, req.body.password);
-    res.sendStatus(result ? 200 : 500);
+    let registered = await userService.registerUser(req.body.username, req.body.email, req.body.password);
+    registered ? res.sendStatus(200) : res.send(500, "Username already exists!");
 });
 
 module.exports = router;
