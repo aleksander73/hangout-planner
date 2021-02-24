a5e140fddbf2c4943497594d9fd2218d5ecd970f
aleksander73 <olekdobkowski@gmail.com>
2021-02-25 00:27:26 +0100

    Add api client

---
+++ frontend/src/api/ApiClient.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+class ApiClient {
+    async userExists(username) {
+        const { data } = await axios.post('/api/user/exists', { username });
+        return data.exists;
+    }
+
+    async registerUser(username, email, password) {
+        const { status } = await axios.post('/api/user/register', { username, email, password });
+        return status === 200;
+    }
+}
+
+module.exports = Object.freeze(new ApiClient());
